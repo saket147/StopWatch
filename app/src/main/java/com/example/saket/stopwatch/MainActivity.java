2ac1f41b9758df63bfe11471d6b308e02fcb2bb7
saket <saket19954@gmail.com>
2016-10-15 01:47:04 +0530

    Added Menu Items

--- app/src/main/java/com/example/saket/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/saket/stopwatch/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -83,4 +86,23 @@ public class MainActivity extends AppCompatActivity {
             time.setTextColor(Color.RED);
             handler.postDelayed(this, 0);
         }};
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.instructions) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
